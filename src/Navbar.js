4be446e3e21ec441c7d6a5a4d141e6378c6719f5
sparsh2002 <76423083+sparsh2002@users.noreply.github.com>
2021-05-20 09:39:52 -0700

    Add files via upload

---
+++ src/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+/*const styles = {color:'white',
+backgroundColor:'#f1356d',
+borderRadius:'8px'}*/
+
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>The Dojo Blog</h1>
+            <div className="links">
+                <Link to="/">Home</Link>
+                <Link to="/create" 
+                style={null}
+                >New Blog</Link>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
